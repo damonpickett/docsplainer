d6f0efa5ae0000f408d3c6f07b117cfb37e73680
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2023-08-30 20:24:05 -0700

    created ChatMessage model

--- backend/docsplainer/admin.py
+++ backend/docsplainer/admin.py
@@ -1,20 +1,8 @@
 from django.contrib import admin
-from .models import Document, ChatMessage, AIResponse
+from .models import ChatMessage  # Import the ChatMessage model from your models.py
 
-@admin.register(Document)
-class DocumentAdmin(admin.ModelAdmin):
-    list_display = ('title', 'user', 'upload_date')
-    list_filter = ('user', 'upload_date')
-    search_fields = ('title', 'user__username')
-    
 @admin.register(ChatMessage)
 class ChatMessageAdmin(admin.ModelAdmin):
-    list_display = ('user', 'timestamp', 'content')
-    list_filter = ('user', 'timestamp')
-    search_fields = ('user__username', 'content')
-
-@admin.register(AIResponse)
-class AIResponseAdmin(admin.ModelAdmin):
-    list_display = ('user', 'timestamp', 'content')
+    list_display = ('user', 'content', 'timestamp')
     list_filter = ('user', 'timestamp')
     search_fields = ('user__username', 'content')
--- backend/docsplainer/models.py
+++ backend/docsplainer/models.py
@@ -1,27 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Document(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    upload_date = models.DateTimeField(auto_now_add=True)
-    pdf_file = models.FileField(upload_to='documents/')
-
-    def __str__(self):
-        return self.title
-
 class ChatMessage(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)  # Allow null for bot messages
     content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username} - {self.timestamp}"
-
-class AIResponse(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    content = models.TextField()
-
-    def __str__(self):
-        return f"{self.user.username} - {self.timestamp}"
+        return f"{self.user.username if self.user else 'Bot'} - {self.timestamp}"
